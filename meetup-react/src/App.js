ab358907bd55243e6802d7e996a084db54978c49
cisu <49948070+cisu@users.noreply.github.com>
2021-05-11 22:00:35 +0300

    Creating _Wrapper_ Components

--- meetup-react/src/App.js
+++ meetup-react/src/App.js
@@ -1,35 +1,28 @@
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
 
-import MainNavigation from './components/layout/MainNavigation';
-
+import Layout from './components/layout/Layout';
 
 function App() {
-
   return (
-    <div>
-
-      <MainNavigation />
-
+    <Layout>
       <Switch>
-
-        <Route path="/" exact={true}>
+        <Route path='/' exact={true}>
           <AllMeetupsPage />
         </Route>
-      
-        <Route path="/new-meetup">
+
+        <Route path='/new-meetup'>
           <NewMeetupPage />
         </Route>
 
-        <Route path="/favorites">
+        <Route path='/favorites'>
           <FavoritesPage />
         </Route>
-
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
