502da436e105e08cdf3d449c10f4a8fc798386e8
cisu <49948070+cisu@users.noreply.github.com>
2021-05-13 00:18:11 +0300

    Navigating Programmatically

--- meetup-react/src/pages/NewMeetup.js
+++ meetup-react/src/pages/NewMeetup.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 import NewMeetupFrom from '../components/meetups/NewMeetupForm'
 
 const NewMeetupPage = () => {
+
+    const history = useHistory();
 
     function addMeetupHandler(meetupData) {
         fetch('https://react-getting-started-1c0bd-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
@@ -14,7 +17,9 @@ const NewMeetupPage = () => {
             }
         }
         
-        )
+        ).then(() => {
+            history.replace('/')
+        })
 
     }
 
