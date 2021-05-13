8aa1989baeb4a18514c4be407aa5399f2f0737e0
cisu <49948070+cisu@users.noreply.github.com>
2021-05-13 13:01:51 +0300

    Updating State Based On Previous State v-2

--- meetup-react/src/store/favorites-context.js
+++ meetup-react/src/store/favorites-context.js
@@ -1,20 +1,37 @@
-import { createContext, useState } from 'react';
+import {createContext, useState} from 'react';
 
-const FavoritesContext =  createContext({
-    favorites: [],
-    totalFavorites: 0
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
 });
 
 function FavoritesContextProvider(props) {
+  const [useFavorites, setUserFavorites] = useState([]);
 
-   const [useFavorites, setUserFavorites] = useState([]);
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites(prevUserFavorites => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
 
-    const context = {
-        favorites: useFavorites,
-        totalFavorites: userFavorites.length,
-    };
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites(prevUserFavorites => {
+        return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+    })
+  }
 
-    return <FavoritesContext.Provider value={context}>
-        {props.children}
+  function itemIsFavoriteHandler(meetupId) {
+      return userFavorites.some(meetup => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: useFavorites,
+    totalFavorites: userFavorites.length,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
     </FavoritesContext.Provider>
+  );
 }
