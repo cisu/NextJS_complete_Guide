7b806383a7bff1840e02b7303665d61bdc2d3a3a
cisu <49948070+cisu@users.noreply.github.com>
2021-05-13 11:50:05 +0300

    Sending a POST Http Request v-2

--- meetup-react/src/pages/NewMeetup.js
+++ meetup-react/src/pages/NewMeetup.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-import NewMeetupFrom from '../components/meetups/NewMeetupForm'
+import NewMeetupFrom from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-b2b09-default-rtdb.firebaseio.com/meetups.json',
+      {
+          method: 'POST',
+          body: JSON.stringify(meetupData),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      }
+    );
+  }
 
-    function addMeetupHandler(meetupData) {
-
-    }
-
-    return (
+  return (
     <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupFrom onAddMeetup={addMeetupHandler}/>
+      <h1>Add New Meetup</h1>
+      <NewMeetupFrom onAddMeetup={addMeetupHandler} />
     </section>
-    )
-}
+  );
+};
 
 export default NewMeetupPage;
