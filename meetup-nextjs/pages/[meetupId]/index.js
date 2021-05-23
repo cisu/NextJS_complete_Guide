0006ec51938c6a8fc165c14d1f7449b3e779fed4
george <geor@webdi.com>
2021-05-23 19:13:59 +0300

    dynamic pages and getStaticPaths

--- meetup-nextjs/pages/[meetupId]/index.js
+++ meetup-nextjs/pages/[meetupId]/index.js
@@ -1,32 +1,52 @@
-import MeetupDetail from '../../components/meetups/MeetupDetail'
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function MeetupDetails() {
   return (
-   <MeetupDetail 
-   title= 'A First Meetup'
-   image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg'
-   address= 'Some address 5, 12345 Some City'
-   description= 'This is a first meetup!'
-   />
+    <MeetupDetail
+      title='A First Meetup !'
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg'
+      address='Some address 5, 12345 Some City'
+      description='This is a first meetup!'
+    />
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
   // fetch data for a single meetup
 
-  const meetupId = context.params.meetups;
+  // .meetupId because that's the identifier i have i between the square brackets i pages/[meetupId]
+  const meetupId = context.params.meetupId;
 
-  console.log(meetupsId);
+  console.log(meetupId);
 
-  props: {
-    meetupData: {
-      image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchenjpg'
-      id: meetupsId
-      title: 'A First Meetup'
-      address: 'Some address 5, 12345 Some City'
-      description: 'This is a first meetup!'
-    }
-  }
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchenjpg',
+        id: meetupId,
+        title: 'A First Meetup',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!',
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- meetup-nextjs/pages/index.js
+++ meetup-nextjs/pages/index.js
@@ -11,7 +11,7 @@ const DUMMY_MEETUPS = [
   },
   {
     id: 'm2',
-    title: 'A First Meetup',
+    title: 'A Second Meetup',
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
     address: 'Some address 10, 12345 Some City3',
